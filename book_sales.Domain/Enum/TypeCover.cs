1d52e96f764b09a30dab26936a9134bc215ab2e8
Vladislav <vladislav0kostin@gmail.com>
2022-06-18 11:27:58 +0300

    Создать перечисление тип обложки

---
+++ book_sales.Domain/Enum/TypeCover.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace book_sales.Domain.Enum
+{
+    public enum TypeCover
+    {
+        [Display(Name = "Твердый переплет")] Hardcover = 0,
+        
+        [Display(Name = "Мягкий переплет")] Softcover = 1,
+        
+        [Display(Name = "Суперобложка")] DustJacket = 2,
+    }
+}
